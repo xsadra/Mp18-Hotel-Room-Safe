1833da3eaec9a1e06e299a73e2c3999098d163c0
Sadra <ti9mail@gmail.com>
2018-04-24 08:16:44 +0200

    Finished mini Pset 18

---
+++ src/main/java/de/sadrab/Mp18HotelRoomSafe/Mp18HotelRoomSafeApplication.java
@@ -0,0 +1,12 @@
+package de.sadrab.Mp18HotelRoomSafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp18HotelRoomSafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp18HotelRoomSafeApplication.class, args);
+	}
+}
